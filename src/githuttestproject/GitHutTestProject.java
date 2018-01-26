0ddbb5dda40c3edfca045e02aeb33b495da06e34
jonas <jones16511@hotmail.com>
2018-01-26 09:48:18 +0100

    Hello World
    Test

--- src/githuttestproject/GitHutTestProject.java
+++ src/githuttestproject/GitHutTestProject.java
@@ -15,7 +15,7 @@ public class GitHutTestProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
